6f924a7e885e0082bbb650292bd5d1c20cb4b3f7
H Sangamesh <sanguhsandy@gmail.com>
2026-09-25 02:09:18 +0530

    Configure frontend for deployed API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://education-support-ticket-management.onrender.com";
 
 const categories = [
   "FEES",

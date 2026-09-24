dd1e9bdb7654d3d47260a52dac5a98f3ab93b734
H Sangamesh <sanguhsandy@gmail.com>
2026-09-25 03:38:03 +0530

    Use PBKDF2 password hashing

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -5,8 +5,9 @@ from passlib.context import CryptContext
 
 from app.core.config import settings
 
+
 pwd_context = CryptContext(
-    schemes=["bcrypt"],
+    schemes=["pbkdf2_sha256"],
     deprecated="auto",
 )
 
